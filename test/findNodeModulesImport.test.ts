52ab9ab43cc620701589ed82d12dc09d27328206
azu <azuciao@gmail.com>
2023-01-13 22:36:02 +0900

    test: add node:prefix

--- test/findNodeModulesImport.test.ts
+++ test/findNodeModulesImport.test.ts
@@ -2,7 +2,47 @@ import { findNodeModulesImport } from "../src/find-node-modules-import.js";
 import assert from "node:assert";
 
 describe("findNodeModulesImport", function () {
-    it("should find the import", async function () {
+    it("should find node:prefix", async function () {
+        const result = await findNodeModulesImport(
+            `\
+import assert from 'node:assert'
+import fs from "fs";
+
+type A = string; // It is typescript
+`
+        );
+        assert.deepStrictEqual(result, [
+            {
+                name: "node:assert",
+                range: [20, 31],
+                loc: {
+                    start: {
+                        line: 1,
+                        column: 20
+                    },
+                    end: {
+                        line: 1,
+                        column: 31
+                    }
+                }
+            },
+            {
+                name: "fs",
+                range: [49, 51],
+                loc: {
+                    start: {
+                        line: 2,
+                        column: 16
+                    },
+                    end: {
+                        line: 2,
+                        column: 18
+                    }
+                }
+            }
+        ]);
+    });
+    it("should find import statement", async function () {
         const result = await findNodeModulesImport(
             `\
 import { foo } from 'bar1'
